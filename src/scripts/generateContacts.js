dd72f5286c8d7ccd10a56dc3ef05fd7e7e9572d1
Anastasia <tildatopdev@gmail.com>
2024-12-01 20:59:32 +0200

    add generate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,16 @@
-const generateContacts = async (number) => {};
+//1створити фейкову кількісь контактів(у utils/createFakeontats)
+//2додавати контакти до попереднього файла нові
+//3. записати контакти у файл debugger.json
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  // const contact = createFakeContact();
+  const contactList = await readContacts(); //зчитуємо контакти, що були,
+  const newContact = Array(number).fill(0).map(createFakeContact); //створюємо пісні нові
+  const newList = [...contactList, ...newContact]; //до попередніх контактів додаємо нові
+  await writeContacts(newList); // старі контакти+нові передаємо в функцію writeContacts
+};
 
 generateContacts(5);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,6 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const readContacts = async () => {};
+import { readFile } from 'node:fs/promises';
+export const readContacts = async () => {
+  const data = await readFile(PATH_DB, 'utf-8'); // читаємо файл db з допомогою пакету fs:
+  return JSON.parse(data); //зі строки робимо масив
+};
