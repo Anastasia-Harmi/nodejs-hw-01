aa18703b183b06fa4d851eb1210246e1fbaefc0f
Anastasia <tildatopdev@gmail.com>
2024-12-01 23:42:02 +0200

    add another scripts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,9 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+//скрипт повертає всі контакти, ігноруємо await, бо функція і так повертає проміс і при виклику ми пишемо await і так
+export const getAllContacts = () => {
+  const allContacts = readContacts();
+  return allContacts;
+};
 
 console.log(await getAllContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,9 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  contacts.pop(); //pop -метод масиву,що вирізає і повертає останній елемент масиву, так як в змінну не зберігаємо - то просто вирізає тут
+  await writeContacts(contacts); //перезаписуємо
+};
 
 removeLastContact();
